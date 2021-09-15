2e9a94c150b43becae52598a0ecc233e6942bb02
Amory Hoste <amory.hoste@gmail.com>
2021-09-15 22:18:28 +0200

    increase metallb memory

--- scripts/cluster/setup_master_node.sh
+++ scripts/cluster/setup_master_node.sh
@@ -37,7 +37,7 @@ sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
+kubectl apply -f $ROOT/configs/metallb/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f $ROOT/configs/metallb/metallb-configmap.yaml
 
